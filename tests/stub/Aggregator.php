193704a84bc2cf405d8951a2cbb68d103e693e6a
bornkes <born-kes@o2.pl>
2020-05-11 19:16:17 +0200

    add function to string

--- tests/stub/Aggregator.php
+++ tests/stub/Aggregator.php
@@ -24,10 +24,10 @@ class Aggregator
      * @param null $arg
      * @return mixed|null
      */
-    public static function getConfigParam ($_method_, $arg=null )
+    public static function getConfigParam (string $_method_, $arg = null )
     {
 
-        self::$call[] = "{$_method_}" . (is_Null($arg)?'':" => {$arg}");
+        self::$call[] = "{$_method_}" . (is_Null($arg)?'':" => ".self::toString($arg) );
 
         if ( isset (self::$config[$_method_]) ){
             return self::$config[$_method_];
@@ -72,5 +72,41 @@ class Aggregator
     public static function unsetAggregator()
     {
         self::$config = self::$call = [];
+    }
+
+    /**
+     * convert value to strong
+     *
+     * @param mixed $value
+     * @return string
+     */
+    public static function toString($value) :string
+    {
+        switch(true) {
+            case is_numeric($value) :
+            case is_string($value) :
+                return $value;
+                break;
+            case $value instanceof Closure:
+                return "function";
+                break;
+            case is_callable($value):
+                return "function";
+                break;
+            case $value instanceof \Base:
+                return "Base";
+                break;
+            case is_object($value):
+                return get_class($value);
+                break;
+            case is_array($value):
+            case is_bool($value):
+                ob_start();
+                $string = ob_get_contents();
+                ob_end_clean();
+                return $string;
+                break;
+        }
+        return  gettype ($value);
     }
 }
