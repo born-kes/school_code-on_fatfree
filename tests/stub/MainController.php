9e8822c5324d673824718422cb22321035f87006
bornkes <born-kes@o2.pl>
2020-05-12 09:55:42 +0200

    add file tests/stub/MainController.php

---
+++ tests/stub/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace stub;
+
+use v1\interfaces\ControllerInterface;
+
+class MainController implements ControllerInterface
+{
+    public function index(\Base $f3)
+    {
+
+    }
+}
